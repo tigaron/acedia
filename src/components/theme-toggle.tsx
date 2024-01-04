d04080c7cbc62413c3147f93d06638d68d397ff4
tigaron <suputra@mail.com>
2024-01-04 22:42:41 +0700

    fix: change bg color

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -9,7 +9,7 @@ export function ThemeToggle() {
   const { setTheme } = useTheme();
 
   return (
-    <Button variant="outline" size="icon">
+    <Button className="bg-transparent border-0" variant="outline" size="icon">
       <Sun
         className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
         onClick={() => setTheme('dark')}

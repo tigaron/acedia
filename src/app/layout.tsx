48acbc05694e796e16570daf94bbe4feb6a12bfa
tigaron <suputra@mail.com>
2024-01-04 11:35:15 +0700

    feat: add clerk as auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
-import './globals.css'
+import './globals.css';
 
-const font = Open_Sans({ subsets: ['latin'] })
+import type { Metadata } from 'next';
+
+import { Open_Sans } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+
+const font = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Acedia',
   description: 'Chat with your friends and family.',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }

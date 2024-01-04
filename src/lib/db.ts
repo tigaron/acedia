fa20386986f3f5c6ee38b6b4b38ef440373f9dea
tigaron <suputra@mail.com>
2024-01-04 16:48:44 +0700

    feat: add onboarding to create first server

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,10 +4,29 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db =
-  global.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  });
+export const db = new PrismaClient({
+  log: [
+    { level: 'query', emit: 'event' },
+    { level: 'warn', emit: 'event' },
+    { level: 'info', emit: 'event' },
+    { level: 'error', emit: 'event' },
+  ],
+});
+
+db.$on('query', e => {
+  console.log(e);
+});
+
+db.$on('warn', e => {
+  console.log(e);
+});
+
+db.$on('info', e => {
+  console.log(e);
+});
+
+db.$on('error', e => {
+  console.log(e);
+});
 
 if (process.env.NODE_ENV !== 'production') global.prisma = db;

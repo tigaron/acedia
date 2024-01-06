84f6eac680f46030ae2799f988c8f91858b56244
tigaron <suputra@mail.com>
2024-01-06 11:27:47 +0700

    feat: add edit channel modal

--- src/app/api/channels/[channelId]/route.ts
+++ src/app/api/channels/[channelId]/route.ts
@@ -51,3 +51,73 @@ export async function DELETE(
     );
   }
 }
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { channelId: string } },
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) return new NextResponse('Unauthorized', { status: 401 });
+
+    if (!params.channelId)
+      return new NextResponse('Channel ID is required', { status: 400 });
+
+    const { searchParams } = new URL(req.url);
+
+    const serverId = searchParams.get('serverId');
+
+    if (!serverId)
+      return new NextResponse('Server ID is required', { status: 400 });
+
+    const { name, type } = await req.json();
+
+    if (!name) return new NextResponse('Name is required', { status: 400 });
+
+    if (!type) return new NextResponse('Type is required', { status: 400 });
+
+    if (name === 'general')
+      return new NextResponse('Cannot use name "general"', { status: 409 });
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: params.channelId,
+              NOT: {
+                name: 'general',
+              },
+            },
+            data: {
+              name,
+              type,
+            },
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server, { status: 200 });
+  } catch (error) {
+    console.error('[Channels_ChannelId][PATCH]', error);
+    return new NextResponse(
+      (error as Error).message || 'Internal Server Error',
+      {
+        status: 500,
+      },
+    );
+  }
+}
--- src/hooks/use-modal-store.ts
+++ src/hooks/use-modal-store.ts
@@ -10,7 +10,8 @@ export type ModalType =
   | 'createChannel'
   | 'leaveServer'
   | 'deleteServer'
-  | 'deleteChannel';
+  | 'deleteChannel'
+  | 'editChannel';
 
 interface ModalData {
   server?: Server;

0722e339697b1ec6a8b534e3433e462f5dbe6934
tigaron <suputra@mail.com>
2024-01-06 14:37:15 +0700

    style: remove db logging

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,29 +4,6 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = new PrismaClient({
-  log: [
-    { level: 'query', emit: 'event' },
-    { level: 'warn', emit: 'event' },
-    { level: 'info', emit: 'event' },
-    { level: 'error', emit: 'event' },
-  ],
-});
-
-db.$on('query', e => {
-  console.log(e);
-});
-
-db.$on('warn', e => {
-  console.log(e);
-});
-
-db.$on('info', e => {
-  console.log(e);
-});
-
-db.$on('error', e => {
-  console.log(e);
-});
+export const db = new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') global.prisma = db;

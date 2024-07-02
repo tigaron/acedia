483f710765372c184d562dff2dbfa453088ed055
tigaron <suputra@mail.com>
2024-07-02 08:17:55 +0700

    fix: disable useAuth lint error

--- src/lib/current-profile.ts
+++ src/lib/current-profile.ts
@@ -3,6 +3,7 @@ import { useAuth } from '@clerk/nextjs';
 import { db } from '@/lib/db';
 
 export async function currentProfile() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const { userId } = useAuth();
 
   if (!userId) return null;
